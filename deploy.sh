f57e02c51dc20528eadea3c5dacf6471387420e5
李启龙 <liql@weipaitang.com>
2021-06-14 22:27:53 +0800

    [添加自动部署到服务器]

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run build
+npm install && npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -24,21 +24,8 @@ else
   git config --global user.name "eryajf"
   git config --global user.email "Linuxlql@163.com"
 fi
+
 git init
 git add -A
 git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
-
-
-# deploy to coding pages
-# echo 'www.eryajf.com\neryajf.com' > CNAME  # 自定义域名
-# echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
-
-# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-#   codingUrl=git@e.coding.net:xgy/xgy.git
-# else
-#   codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
-# fi
-# git add -A
-# git commit -m "${msg}"
-# git push -f $codingUrl master # 推送到coding
