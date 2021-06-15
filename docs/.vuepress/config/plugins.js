886cf9c6d91f903dbabd21dc86504f568788f869
李启龙 <liql@weipaitang.com>
2021-06-15 21:33:45 +0800

    添加了鼠标点击特效

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -1,13 +1,7 @@
 // 插件配置
 module.exports = [
-  // 本地插件
-  // [require('./plugins/love-me'), { // 鼠标点击爱心特效
-  //   color: '#11a8cd', // 爱心颜色，默认随机色
-  //   excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
-  // }],
-
   'vuepress-plugin-baidu-autopush', // 百度自动推送
-
+  'cursor-effects',
   // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
   [
     'thirdparty-search',
