46cc5c119da2d0cab65e17fc9ce1bc94d16054e8
李启龙 <liql@weipaitang.com>
2021-06-15 21:47:02 +0800

    添加百度统计

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -7,3 +7,23 @@ export default ({
 }) => {
   // window.Vue = vue // 使页面中可以使用Vue构造函数 （使页面中的vue demo生效）
 }
+
+export default ({ router }) => {
+  /**
+   * 路由切换事件处理
+   */
+  router.beforeEach((to, from, next) => {
+    console.log("切换路由", to.fullPath, from.fullPath);
+
+    //触发百度的pv统计
+    if (typeof _hmt != "undefined") {
+      if (to.path) {
+        _hmt.push(["_trackPageview", to.fullPath]);
+        console.log("上报百度统计", to.fullPath);
+      }
+    }
+
+    // continue
+    next();
+  });
+};
