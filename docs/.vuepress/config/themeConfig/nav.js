3241a7fd27e5ed290b72f99f05f45edda8e5699c
eryajf <Linuxlql@163.com>
2021-07-10 00:22:47 +0800

    [调整导航,新增go笔记]

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -1,11 +1,54 @@
 // nav
 module.exports = [
   { text: '首页', link: '/' },
-  { text: '运维',link: '/ops/',},
-  { text: '专题',link: '/topic/',},
-  { text: '生活',link: '/life/'},
-  { text: '编程',link: '/code/'},
-  { text: '周刊', link: '/pages/11c668/' },
+  { text: '运维',link: '/ops/',
+    items: [
+      { text: '最佳实践', link: '/pages/027a17/'},
+      { text: '迎刃而解', link: '/pages/29.html'},
+      { text: 'Nginx', link: '/pages/930.html'},
+      { text: 'Php', link: '/pages/2782.html'},
+      { text: 'Zabbix', link: '/pages/596.html'},
+      { text: 'Prometheus', link: '/pages/2468.html'},
+      { text: 'Grafana', link: '/pages/5380.html'},
+      { text: 'CentOS', link: '/pages/5279.html'},
+      { text: 'Docker', link: '/pages/1847.html'},
+      { text: 'Ansible', link: '/pages/5173.html'},
+      { text: 'Ldap', link: '/pages/3410.html'},
+      { text: 'Gitlab', link: '/pages/243.html'},
+      { text: 'Kafka', link: '/pages/2951.html'},
+      { text: 'Etcd', link: '/pages/4050.html'},
+      { text: 'Other', link: '/pages/285.html'},
+    ]
+  },
+  { text: '专题',link: '/topic/',
+    items: [
+      { text: 'ELK', link: '/pages/2351.html'},
+      { text: 'K8S', link: '/pages/1742.html'},
+      { text: 'Nexus', link: '/pages/45c5da/'},
+      { text: 'Jenkins', link: '/pages/56.html'},
+    ]
+  },
+  { text: '生活',link: '/life/',
+    items: [
+      { text: '随写编年', link: '/pages/2714.html'},
+      { text: '家人物语', link: '/pages/2442.html'},
+      { text: '电影音乐', link: '/pages/184.html'},
+      { text: '效率工具', link: '/pages/136.html'},
+      { text: '博客相关', link: '/pages/844.html'},
+    ]
+  },
+  { text: '编程',link: '/code/',
+    items: [
+      { text: 'Vue', link: '/pages/5164.html'},
+      { text: 'Shell', link: '/pages/531.html'},
+      { text: 'Golang', link: '/pages/aeab08/'},
+    ]
+  },
+  { text: '周刊', link: '/pages/11c668/',
+    items: [
+      { text: '2021', link: '/pages/11c668/'},
+    ]
+  },
   { text: '关于', link: '/about/' },
   { text: '索引', link: '/categories/',
     items: [
@@ -16,9 +59,10 @@ module.exports = [
   },
   {
     text: '页面',
-    link: '/pages/ba90ae/',
+    link: '/friends/',
     items: [
-      { text: '友链', link: '/pages/ba90ae/' },
+      { text: '友链', link: '/friends/' },
+      { text: '导航', link: '/nav/' },
     ],
   },
 ]
