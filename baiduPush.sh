9b66090f2a9d5872841dbc0941ffce0ca7e06041
eryajf <eryajf@163.com>
2022-11-25 10:05:53 +0800

    调整一些内容

--- baiduPush.sh
+++ baiduPush.sh
@@ -6,4 +6,7 @@ set -e
 # 百度链接推送
 curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://wiki.eryajf.net&token=E1DGYsPYcM8svN1I"
 
+# 必应链接推送
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "https://ssl.bing.com/webmaster/api.svc/json/GetUrlSubmissionQuota?siteUrl=https://wiki.eryajf.net&apikey=5442880bca524d11b94911d5e9b89a11"
+
 rm -rf urls.txt # 删除文件
--- sync.sh
+++ sync.sh
@@ -4,6 +4,9 @@ set -e
 # 编译
 # pnpm install
 pnpm build
+
+# 必应认证
+cp utils/BingSiteAuth.xml docs/.vuepress/dist
 # 同步
 rsync -az --delete docs/.vuepress/dist/  \
     root@8.136.215.57:/data/www/eryajf.github.io/
